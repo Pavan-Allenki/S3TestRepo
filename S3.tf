9f56a76f74e23e69fb1358311e6f8ca5520881f4
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-06-09 22:47:27 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -10,3 +10,30 @@ resource "aws_s3_bucket" "unrestricted" {
     target_prefix = "log/"
     }
 }
+
+resource "aws_db_subnet_group" "km_rds_subnet_grp" {
+  name       = "km_rds_subnet_grp_${var.environment}"
+  subnet_ids = var.private_subnet
+
+  tags = merge(var.default_tags, {
+    Name = "km_rds_subnet_grp_${var.environment}"
+  })
+}
+
+resource "aws_kms_key" "km_db_kms_key" {
+  description             = "KMS Key for DB instance ${var.environment}"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+
+  tags = merge(var.default_tags, {
+    Name = "km_db_kms_key_${var.environment}"
+  })
+}
+
+resource "aws_s3_bucket" "km_blob_storage" {
+  bucket = "km-blob-storage-${var.environment}"
+  acl    = "private"
+  tags = merge(var.default_tags, {
+    name = "km_blob_storage_${var.environment}"
+  })
+}
